d3cdb9ae0116e8fbe5d7439816640746e0641b52
tavonunez260 <tavonunez260@gmail.com>
2022-03-22 00:51:40 -0500

    navbar rev1 finished

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ class NavBar {
   header = document.querySelector('.header')
   navbarObserverOptions = {
     root: null,
-    threshold: [0.95, 0.05],
+    threshold: [0.95, 0.01],
   }
   constructor () {
     this.responsiveButton.addEventListener('click', this._deployResponsiveMenu.bind(this))
@@ -22,10 +22,12 @@ class NavBar {
     if(!this.open) {
       this.navbarItems.classList.add('navbar__items--active')
       this.navbarItems.classList.remove('navbar__items--inactive')
+      body.classList.add('hide-overflow')
       this._toggleOpen()
     } else {
       this.navbarItems.classList.remove('navbar__items--active')
       this.navbarItems.classList.add('navbar__items--inactive')
+      body.classList.remove('hide-overflow')
       this._toggleOpen()
     }
   }
@@ -37,35 +39,39 @@ class NavBar {
     if (entry.intersectionRatio <= 0.95 && entry.isIntersecting) {
       this.navbarLayout.classList.add('scroll')
       this.navbarLayout.classList.remove('non-scroll')
-
-      this.navbarItems.classList.add('navbar__items--scroll')
-      this.navbarItems.classList.remove('navbar__items--non-scroll')
-
+      this.navbarItems.classList.add('scroll')
+      this.navbarItems.classList.remove('non-scroll')
       this.navbarLink.forEach(link => link.classList.add('scroll'))
       this.navbarLink.forEach(link => link.classList.remove('non-scroll'))
-
       this.navbarElement.forEach(link => link.classList.add('scroll'))
-
       this.navbarLogoImgTop.classList.add('navbar__logo_img--hide')
       this.navbarLogoImgBottom.classList.add('navbar__logo_img--show')
     } else {
       this.navbarLayout.classList.remove('scroll')
       this.navbarLayout.classList.add('non-scroll')
-
-      this.navbarItems.classList.remove('navbar__items--scroll')
-      this.navbarItems.classList.add('navbar__items--non-scroll')
-
+      this.navbarItems.classList.remove('scroll')
+      this.navbarItems.classList.add('non-scroll')
       this.navbarLink.forEach(link => link.classList.remove('scroll'))
       this.navbarLink.forEach(link => link.classList.add('non-scroll'))
-
       this.navbarElement.forEach(link => link.classList.remove('scroll'))
-
       this.navbarLogoImgTop.classList.remove('navbar__logo_img--hide')
       this.navbarLogoImgBottom.classList.remove('navbar__logo_img--show')
     }
 
     if (entry.intersectionRatio <= 0.01 && !entry.isIntersecting) {
-      console.log('I am off the screen!')
+      this.navbarLayout.classList.add('fixed')
+      this.navbarLogoImgBottom.classList.add('fixed')
+      this.navbarItems.classList.remove('non-scroll')
+      this.navbarItems.classList.add('fixed')
+      this.navbarLink.forEach(link => link.classList.remove('non-scroll'))
+      this.navbarLink.forEach(link => link.classList.add('fixed'))
+
+    } else {
+      this.navbarLayout.classList.remove('fixed')
+      this.navbarLogoImgBottom.classList.remove('fixed')
+      this.navbarItems.classList.remove('fixed')
+      this.navbarLink.forEach(link => link.classList.remove('fixed'))
+
     }
   }
   _toggleOpen = () => {
